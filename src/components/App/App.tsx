ea6d3c9e021ad66a15e6183702d2720c85348e3c
Anatolii Honchar <gonchar_tol@ukr.net>
2026-03-17 18:26:57 +0200

    refactor: update App component structure and styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
+import css from "./App.module.css";
+
 export default function App() {
   console.log("App component rendered");
 
   return (
-    <div className="app">
-      <h1>NoteHub</h1>
-      <p>Welcome to NoteHub!</p>
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
     </div>
   );
 }
