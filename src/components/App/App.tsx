7d8ea28faab5d5f855a5c6bfd5e6efc9ca106c82
Anatolii Honchar <gonchar_tol@ukr.net>
2026-03-20 22:54:23 +0200

    feat: integrate @tanstack/react-query for data fetching and create NoteList component for rendering notes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../services/noteService";
+import NoteList from "../NoteList/NoteList";
 import css from "./App.module.css";
 
 export default function App() {
-  console.log("App component rendered");
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["notes"],
+    queryFn: () => fetchNotes(1, ""),
+  });
+
+  const notes = data?.notes ?? [];
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        {/* Компонент SearchBox */}
-        {/* Пагінація */}
-        {/* Кнопка створення нотатки */}
+        {/* SearchBox */}
+        {/* Pagination */}
+        {/* Button */}
       </header>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error loading notes</p>}
+
+      {notes.length > 0 && <NoteList notes={notes} />}
     </div>
   );
 }
